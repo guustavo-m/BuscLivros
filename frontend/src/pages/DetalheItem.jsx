8fd5e7c38ec7b473a1cb8362ae70299fcd2fb4e2
gabriellacstavarengo <gabriellacstavarengo@gmail.com>
2026-09-15 08:09:48 -0300

    refactor: remove unnecessary React import statement in DetalheItem component

--- frontend/src/pages/DetalheItem.jsx
+++ frontend/src/pages/DetalheItem.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaArrowLeft, FaHeart, FaStar } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom";
 
